d3eca51da6e6dd992373c8a3faed73066b99e5b3
Akhlaq <imakhlaqXD@gmail.com>
2022-08-01 23:40:20 +0530

    name

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,8 +59,8 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Akhlaq Ahmad.</h2>;
+  const three = <h3 className="big-heading">I build things for fun.</h3>;
   const four = (
     <>
       <p>
@@ -80,7 +80,7 @@ const Hero = () => {
       href="https://www.newline.co/courses/build-a-spotify-connected-app"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Talk To Me!
     </a>
   );
 
